9e1ce3838636d6610f7ff8d9bf8ac8f6d75f2804
Luke Leheny <luke.leheny@pm.me>
2024-05-07 15:42:51 -0400

    Add more dconf settings

--- user/home/dconf.nix
+++ user/home/dconf.nix
@@ -9,10 +9,18 @@
     "org/gnome/desktop/interface" = {
       "clock-format" = "12h";
       "color-scheme" = "prefer-dark";
+      "cursor-theme" = "OpenZone_Black";
       "document-font-name" = "FreeSans 11";
-      "gtk-theme" = "Adwaita-dark";
       "font-name" = "FreeSans 11";
-      "monospace-font-name" = "Fantasque Sans Mono Bold 11";
+      "gtk-theme" = "Adwaita-dark";
+      "enable-hot-corners" = false;
+      "monospace-font-name" = "Fantasque Sans Mono Bold 12";
+    };
+    "org/gnome/desktop/notifications" = {
+      "show-in-lock-screen" = false;
+    };
+    "org/gnome/desktop/peripherals/mouse" = {
+      "accel-profile" = "flat";
     };
     "org/gnome/desktop/sound" = {
       "event-sounds" = false;
@@ -24,6 +32,14 @@
     };
     "org/gnome/mutter" = {
       "center-new-windows" = true;
+      "dynamic-workspaces" = true;
+    };
+    "org/gnome/desktop/notifications" = {
+      "show-banners" = false;
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      "power-button-action" = "interactive";
+      "sleep-inactive-ac-type" = "nothing";
     };
   };
 }
