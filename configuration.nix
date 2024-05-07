eb6fc0ddd3c73565e1e3285349595fffdbf62213
Luke Leheny <luke.leheny@pm.me>
2024-05-07 15:43:46 -0400

    Update syntax for unstable

--- configuration.nix
+++ configuration.nix
@@ -32,12 +32,10 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome = {
-    enable = true;
-  };
-  services.xserver = {
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
   services.printing.enable = true;
 
