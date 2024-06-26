9052c475180958a6b9c818d018f80ed5df7c95ae
Luke Leheny <luke.leheny@pm.me>
2024-06-25 21:41:03 -0400

    Add config for obs virtual cam

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,11 @@
     "hid-nintendo"
     "v4l2loopback"
   ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
