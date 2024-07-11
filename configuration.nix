64344ede5102f747dcff4170ed65ded75b373fe1
Luke Leheny <luke.leheny@pm.me>
2024-07-10 23:33:06 -0400

    Update nf command

--- configuration.nix
+++ configuration.nix
@@ -74,7 +74,7 @@
     mb = "cd /home/luke && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup";
     nb = "sudo nixos-rebuild boot";
     nc = "sudo nix-collect-garbage --delete-older-than 14d";
-    nf = "nu && nr && nc";
+    nf = "mb && nu && nr && nc";
     nr = "sudo nixos-rebuild switch";
     nu = "sudo nix-channel --update";
   };
