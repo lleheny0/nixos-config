b8c1133317f1c044c597582ab0693a89bcaf5ac4
Luke Leheny <luke.leheny@pm.me>
2024-08-04 15:50:24 -0400

    Disable audio power saving

--- modules/tv.nix
+++ modules/tv.nix
@@ -1,4 +1,8 @@
 {
+  boot.extraModprobeConfig = ''
+    options snd_hda_intel power_save=0
+  '';
+
   home-manager.users.luke = { lib, ... }: {
     dconf.settings = {
       "org/gnome/desktop/session" = {
