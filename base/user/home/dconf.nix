e4c2803df747cd6a80aa5ae1364e0165888b1648
Luke Leheny <luke.leheny@pm.me>
2024-08-09 09:55:58 -0400

    Add fractional scaling

--- base/user/home/dconf.nix
+++ base/user/home/dconf.nix
@@ -49,6 +49,7 @@
         center-new-windows = true;
         dynamic-workspaces = true;
         edge-tiling = true;
+        experimental-features = ["scale-monitor-framebuffer"];
         workspaces-only-on-primary = true;
       };
       "org/gnome/nautilus/icon-view" = {
