4d58300fb1150fca0a309ef5fbd3c778ecc7b86a
Luke Leheny <luke.leheny@pm.me>
2024-08-10 13:55:27 -0400

    Separate mac-input from macbook

---
+++ modules/mac-input.nix
@@ -0,0 +1,12 @@
+{
+  home-manager.users.luke.programs.firefox.profiles.default.settings = {
+    "mousewheel.default.delta_multiplier_y" = 25;
+  };
+
+  services.keyd.keyboards.default.settings.main = {
+    fn = "layer(aux)";
+    leftmeta = "layer(alt)";
+    leftalt = "layer(meta)";
+    leftcontrol = "leftcontrol";
+  };
+}
--- systems/macbook.nix
+++ systems/macbook.nix
@@ -3,27 +3,10 @@
 {
   imports = [
     <nixos-hardware/apple/macbook-pro/12-1>
+    ../modules/mac-input.nix
   ];
 
-  home-manager.users.luke = {
-    dconf.settings = {
-      "org/gnome/desktop/interface" = {
-        cursor-theme = "Adwaita";
-      };
-    };
-    programs.firefox.profiles.default.settings = {
-      "mousewheel.default.delta_multiplier_y" = 25;
-    };
-  };
-
   networking.hostName = "macbookpro";
-
-  services.keyd.keyboards.default.settings.main = {
-    fn = "layer(aux)";
-    leftmeta = "layer(alt)";
-    leftalt = "layer(meta)";
-    leftcontrol = "leftcontrol";
-  };
 
   services.mbpfan = {
     settings.general = {
@@ -31,6 +14,14 @@
       max_temp = 85;
       low_temp = 45;
       high_temp = 65;
+    };
+  };
+
+  home-manager.users.luke = {
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        cursor-theme = "Adwaita";
+      };
     };
   };
 }
