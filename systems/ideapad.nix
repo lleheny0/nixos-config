6e85b32cf8bff37c94d0be0c37d545ccb411952f
Luke Leheny <luke.leheny@pm.me>
2024-08-10 23:02:19 -0400

    Adjust night light for ideapad

--- systems/ideapad.nix
+++ systems/ideapad.nix
@@ -5,4 +5,12 @@
   ];
 
   networking.hostName = "ideapad";
+
+  home-manager.users.luke = { lib, ... }: {
+    dconf.settings = {
+      "org/gnome/settings-daemon/plugins/color" = {
+        night-light-temperature = lib.hm.gvariant.mkUint32 2700;
+      };
+    };
+  };
 }
