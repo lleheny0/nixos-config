b85e68cc9603fda4ac7180135aa5b18ed4881018
Luke Leheny <luke.leheny@pm.me>
2024-08-17 12:47:45 -0400

    Use boot instead of switch in pipeline

--- systems/base/environment.nix
+++ systems/base/environment.nix
@@ -6,7 +6,7 @@
     mb = "cd /home/luke && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup";
     nb = "sudo nixos-rebuild boot";
     nc = "sudo nix-collect-garbage --delete-older-than 14d";
-    nf = "mb && nu && nr && nc";
+    nf = "mb && nu && nb && nc";
     nr = "sudo nixos-rebuild switch";
     nu = "sudo nix-channel --update";
   };
