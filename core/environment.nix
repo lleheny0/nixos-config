3c5f5c9cfde1d37919e4d9166fd6ec21dedd74d8
Luke Leheny <luke.leheny@pm.me>
2024-10-13 11:01:42 -0400

    Add image rename command

--- core/environment.nix
+++ core/environment.nix
@@ -3,6 +3,7 @@
 {
   environment.shellAliases = {
     ll = "ls -alF";
+    ir = "exiftool '-filename<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le' .";
     mb = "cd /home/luke && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup";
     nb = "sudo nixos-rebuild boot";
     nc = "sudo nix-collect-garbage --delete-older-than 14d";
