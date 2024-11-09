85b4070a70e0f308044685dd0e528e286dde109f
Luke Leheny <luke.leheny@pm.me>
2024-11-09 17:36:47 -0500

    Switch to automatic gc

--- core/environment.nix
+++ core/environment.nix
@@ -7,9 +7,7 @@
     ir = "exiftool '-filename<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le' .";
     mb = "cd /home/luke && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup";
     nb = "sudo nixos-rebuild boot";
-    ncu = "nix-collect-garbage --delete-older-than 30d";
-    ncs = "sudo nix-collect-garbage --delete-older-than 30d";
-    nf = "mb && nu && ncs && ncu && nb";
+    nf = "mb && nu && nb";
     nr = "sudo nixos-rebuild switch";
     nu = "sudo nix-channel --update";
   };
--- core/nix.nix
+++ core/nix.nix
@@ -2,4 +2,10 @@
   nix.settings = {
     auto-optimise-store = true;
   };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
