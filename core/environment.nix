9e6417fba4362c5ec355e6ef85a056fb2d913ec8
Luke Leheny <luke.leheny@pm.me>
2024-11-20 00:25:43 -0500

    Suppress pushd & popd output

--- core/environment.nix
+++ core/environment.nix
@@ -5,7 +5,7 @@
     dw = "dconf watch /";
     ll = "ls -alF";
     ir = "exiftool '-filename<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le' .";
-    mb = "pushd /home/luke && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup && popd";
+    mb = "pushd /home/luke > /dev/null && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup && popd > /dev/null";
     nb = "sudo nixos-rebuild boot";
     nf = "mb && nu && nb";
     nr = "sudo nixos-rebuild switch";
