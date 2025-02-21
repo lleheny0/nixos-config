5b2a2b91f5647d0614a0b65f541c3dc5f3df7758
Luke Leheny <luke.leheny@pm.me>
2025-02-20 19:30:58 -0500

    Update to cemu unstable again
    Add note about avoiding stutter
    Stable cemu did not recognize GPU

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }:
 
 # Uncomment the commented lines to switch to stable cemu
-let
-  stable = import
-    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/24.05)
-    { config = config.nixpkgs.config; };
-in
+# Note: Run cemu on a 60hz monitor to avoid stutter
+
+# let
+#   stable = import
+#     (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/24.05)
+#     { config = config.nixpkgs.config; };
+# in
 {
   boot.kernelModules = [
     "hid-nintendo"
@@ -18,8 +20,8 @@ in
   programs.steam.enable = true;
 
   users.users.luke.packages = with pkgs; [
-    stable.cemu
-    # cemu
+    # stable.cemu
+    cemu
     glfw3-minecraft
     joycond-cemuhook
     mcaselector
