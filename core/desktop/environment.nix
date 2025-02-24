7cdeeb7a2566d58c1da7a721476faf379f8032a6
Luke Leheny <luke.leheny@pm.me>
2025-02-24 14:34:29 -0500

    Move more commands to desktop environment.nix

--- core/desktop/environment.nix
+++ core/desktop/environment.nix
@@ -1,6 +1,8 @@
 {
   environment = {
     shellAliases = {
+      dw = "dconf watch /";
+      ir = "exiftool '-filename<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le' .";
       mb = "pushd /home/luke > /dev/null && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup && popd > /dev/null";
       nf = "mb && nu && nb";
     };
--- core/environment.nix
+++ core/environment.nix
@@ -2,9 +2,7 @@
 
 {
   environment.shellAliases = {
-    dw = "dconf watch /";
     ll = "ls -alF";
-    ir = "exiftool '-filename<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le' .";
     nb = "sudo nixos-rebuild boot";
     nr = "sudo nixos-rebuild switch";
     nu = "sudo nix-channel --update";
