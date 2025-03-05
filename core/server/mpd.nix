d0e789f63ff32ce224048947830aeaa9200ac293
Luke Leheny <luke.leheny@pm.me>
2025-03-05 15:08:38 -0500

    Add VGM maintenance scripts

--- core/server/mpd.nix
+++ core/server/mpd.nix
@@ -4,6 +4,34 @@
   environment.systemPackages = with pkgs; [
     mp3gain
     mpc
+    (writeShellApplication {
+      name = "vgmupdate";
+      runtimeInputs = [ mpc ];
+      text = ''
+        mpc stop -q
+        mpc clear -q
+        mpc update --wait -q
+        cd /home/luke/Music/VGM
+        find -- * -mindepth 1 | grep mp3 | mpc --wait add
+        mpc play
+        sudo systemctl restart mpd
+
+        FILE_COUNT=$(find -- * -mindepth 1 | grep -c mp3)
+        MPC_COUNT=$(mpc playlist | wc -l)
+        echo "File count:     $FILE_COUNT"
+        echo "Playlist count: $MPC_COUNT"
+        echo "Done!"
+      '';
+    })
+    (writeShellApplication {
+      name = "vgmgain";
+      runtimeInputs = [ mp3gain ];
+      text = ''
+        mpc stop
+        find "$(pwd)" -type f -iname '*.mp3' -exec mp3gain -r -c -s i {} +
+        mpc play
+      '';
+    })
   ];
 
   services.mpd = {
