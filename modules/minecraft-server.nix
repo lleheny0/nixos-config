3f622fb4b0b325e4c83f2137869649aeb99e9340
Luke Leheny <luke.leheny@pm.me>
2025-03-05 22:41:10 -0500

    Add minecraft server module

---
+++ modules/minecraft-server.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  users.users.luke.packages = with pkgs; [
+    jdk21_headless
+  ];
+
+  systemd.services.minecraft-backup = {
+    description = "Backup the Minecraft folder";
+    script = ''
+      #!/bin/bash
+      timestamp=$(date +%Y-%m-%d)
+      tar -czvf "/home/luke/Backups/Minecraft/$timestamp.tar.gz" -C /home/luke/Minecraft .
+      ls -F /home/luke/Backups/Minecraft/*.tar.gz | head -n -7 | xargs -r rm
+      # Set this up after figuring out the secondary drive
+      # cp -rv /home/luke/Backups/Minecraft /mnt/hdd/backups
+      # ls -F /mnt/hdd/Backups/Minecraft/*.tar.gz | head -n -30 | xargs -r rm
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "luke";
+    };
+  };
+  systemd.timers.minecraft-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 04:00:00";
+      Persistent = true;
+      Unit = "minecraft-backup.service";
+    };
+  };
+}
