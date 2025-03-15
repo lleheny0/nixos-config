faefac82fd0989126fd681b7762b0f5c2ff9c3b8
Luke Leheny <luke.leheny@pm.me>
2025-03-15 00:56:19 -0400

    Remove npm config
    New approach is avoid using -g

--- modules/development/javascript.nix
+++ modules/development/javascript.nix
@@ -4,10 +4,4 @@
   users.users.luke.packages = with pkgs; [
     nodejs
   ];
-
-  home-manager.users.luke.home.file.".npmrc".source = ./dotfiles/.npmrc;
-
-  home-manager.users.luke.home.sessionPath = [
-    "/home/luke/.npm-global"
-  ];
 }
