3df115fdfa48739945fa1c427ad564fc47325919
Luke Leheny <luke.leheny@pm.me>
2025-03-20 19:10:32 -0400

    Add config for slippi to work
    Remove bluetooth ERTM. After more testing that did not fix the Xbox
    controller issue

--- modules/desktop/gaming.nix
+++ modules/desktop/gaming.nix
@@ -9,19 +9,28 @@
 #     { config = config.nixpkgs.config; };
 # in
 {
+  boot.extraModulePackages = [ config.boot.kernelPackages.gcadapter-oc-kmod ];
+
   boot.kernelModules = [
     "hid-nintendo"
+    "gcadapter_oc"
   ];
 
-  boot.kernelParams = [ "bluetooth.disable_ertm=1" ];
+  services.udev.packages = [ pkgs.dolphin-emu ];
 
+  # For the Slippi commands to work, run the AppImage from slippi.gg
   environment.shellAliases = {
     jc = "sudo joycond-cemuhook";
+    sl = "appimage-run /home/luke/Programs/Slippi-Launcher-2.11.8-x86_64.AppImage";
+    sn = "appimage-run /home/luke/.config/Slippi\\ Launcher/netplay/Slippi_Online-x86_64.AppImage";
   };
 
   programs.steam.enable = true;
 
   users.users.luke.packages = with pkgs; [
+    (appimage-run.override {
+      extraPkgs = pkgs: [ curl ];
+    })
     # stable.cemu
     cemu
     glfw3-minecraft
