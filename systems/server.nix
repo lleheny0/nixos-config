560bf479574848e74eae17f485bba3f763eceee2
Luke Leheny <luke.leheny@pm.me>
2025-03-21 16:56:30 -0400

    Add server system

---
+++ systems/server.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ../core/server
+    ../modules/hardware/ignore-lid.nix
+    ../modules/server/minecraft-server.nix
+    ../modules/server/vgm-radio.nix
+  ];
+
+  networking.hostName = "server";
+}
