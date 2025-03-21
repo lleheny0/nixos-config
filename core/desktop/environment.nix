58bc1366372ee853771fc9c404ee3d84d6b676dc
Luke Leheny <luke.leheny@pm.me>
2025-03-21 00:05:54 -0400

    Clean up environment files

--- core/desktop/environment.nix
+++ core/desktop/environment.nix
@@ -6,6 +6,7 @@
       mb = "pushd /home/luke > /dev/null && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup && popd > /dev/null";
       nf = "mb && nu && nb";
     };
+
     variables = {
       BROWSER = "firefox";
     };
--- core/environment.nix
+++ core/environment.nix
@@ -1,17 +1,16 @@
 { lib, pkgs, ... }:
 
 {
-  environment.shellAliases = {
-    ll = "ls -alF";
-    nb = "sudo nixos-rebuild boot";
-    nr = "sudo nixos-rebuild switch";
-    nu = "sudo nix-channel --update";
-  };
+  environment = {
+    shellAliases = {
+      ll = "ls -alF";
+      nb = "sudo nixos-rebuild boot";
+      nr = "sudo nixos-rebuild switch";
+      nu = "sudo nix-channel --update";
+    };
 
-  environment.variables = {
-    EDITOR = "vim";
+    variables = {
+      EDITOR = "vim";
+    };
   };
-
-  environment.systemPackages = with pkgs; [
-  ];
 }
