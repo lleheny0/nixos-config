a10c026136a836a1a20eb503809d3722bc9c70f4
Luke Leheny <luke.leheny@pm.me>
2025-03-27 10:12:28 -0400

    Get slippi appimage fully working

--- modules/desktop/slippi.nix
+++ modules/desktop/slippi.nix
@@ -3,10 +3,11 @@
 # Prerequisites:
 # 1. Download the AppImage from https://slippi.gg/
 # 2. Move it to /home/luke/Programs
-# 3. Run it with appimage-run and log in
 
 {
-  boot.extraModulePackages = [ config.boot.kernelPackages.gcadapter-oc-kmod ];
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.gcadapter-oc-kmod
+  ];
 
   boot.kernelModules = [
     "gcadapter_oc"
@@ -14,14 +15,18 @@
 
   services.udev.packages = [ pkgs.dolphin-emu ];
 
-  users.users.luke.packages = with pkgs; [
-    (appimage-run.override {
-      extraPkgs = pkgs: [ curl ];
-    })
-  ];
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+
+  programs.appimage.package = pkgs.appimage-run.override {
+    extraPkgs = with pkgs; pkgs: [
+      curl
+    ];
+  };
 
   environment.shellAliases = {
     sl = "appimage-run /home/luke/Programs/Slippi-Launcher-2.11.8-x86_64.AppImage";
-    sn = "appimage-run /home/luke/.config/Slippi\\ Launcher/netplay/Slippi_Online-x86_64.AppImage";
   };
 }
