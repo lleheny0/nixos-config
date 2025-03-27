7e14c2aaf19cffed3aeb6487724d86ea9c882075
Luke Leheny <luke.leheny@pm.me>
2025-03-27 10:13:28 -0400

    Add www to url

--- core/desktop/applications/ublock-origin.nix
+++ core/desktop/applications/ublock-origin.nix
@@ -22,7 +22,7 @@
           ! Youtube ambient mode
           www.youtube.com###cinematics
           www.youtube.com###shorts-cinematic-container
-          youtube.com##+js(set, yt.config_.EXPERIMENT_FLAGS.kevlar_watch_cinematics, false)
+          www.youtube.com##+js(set, yt.config_.EXPERIMENT_FLAGS.kevlar_watch_cinematics, false)
 
           ! Youtube shorts
           www.youtube.com##[is-shorts]
