c005de8cdcd7294a34466614425e39f3c5ff4122
Luke Leheny <luke.leheny@pm.me>
2025-04-01 23:11:30 -0400

    Remove systemctl from script

--- modules/server/vgm-scripts.nix
+++ modules/server/vgm-scripts.nix
@@ -16,9 +16,7 @@
       name = "vg";
       runtimeInputs = [ mp3gain ];
       text = ''
-        sudo systemctl stop vgm-stream
         find "$(pwd)" -type f -iname '*.mp3' -exec mp3gain -r -c -s i {} +
-        sudo systemctl start vgm-stream
       '';
     })
   ];
