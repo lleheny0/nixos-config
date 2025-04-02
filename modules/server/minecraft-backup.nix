9e43926e70003471c0a97581ef99c82a4c3f8837
Luke Leheny <luke.leheny@pm.me>
2025-04-02 08:33:08 -0400

    Up backups to 10 days

--- modules/server/minecraft-backup.nix
+++ modules/server/minecraft-backup.nix
@@ -9,9 +9,9 @@
     ];
     script = ''
       tar -czf "/home/luke/Backups/Minecraft/$(date +%Y-%m-%d).tar.gz" -C /home/luke/Minecraft .
-      ls -F /home/luke/Backups/Minecraft/*.tar.gz | head -n -7 | xargs -r rm
+      ls -F /home/luke/Backups/Minecraft/*.tar.gz | head -n -10 | xargs -r rm
       cp -rnv /home/luke/Backups/Minecraft /backup
-      ls -F /backup/Minecraft/*.tar.gz | head -n -7 | xargs -r rm
+      ls -F /backup/Minecraft/*.tar.gz | head -n -10 | xargs -r rm
     '';
     serviceConfig = {
       Type = "oneshot";
