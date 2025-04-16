70b0b0d41c4a8a07a9fe8b04b31e573f0c3b5ce3
Luke Leheny <luke.leheny@pm.me>
2025-04-16 10:33:41 -0400

    Add git aliases

--- core/git.nix
+++ core/git.nix
@@ -6,4 +6,15 @@
       userEmail = "luke.leheny@pm.me";
     };
   };
+
+  environment.shellAliases = {
+    gco = "git checkout";
+    gci = "git commit";
+    gfe = "git fetch";
+    glg = "git log --oneline --graph --all --decorate";
+    gpf = "git push --force-with-lease";
+    gpl = "git pull";
+    gps = "git push";
+    gst = "git status";
+  };
 }
