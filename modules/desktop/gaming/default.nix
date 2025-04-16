16835e7a62d6e7a79d7f4d86e6a14ae0a8c2dd21
Luke Leheny <luke.leheny@pm.me>
2025-04-16 09:59:28 -0400

    Reorganize gaming module

---
+++ modules/desktop/gaming/default.nix
@@ -0,0 +1,9 @@
+{
+  imports = [
+    ./cemu.nix
+    ./minecraft-client.nix
+    ./mangohud.nix
+    ./slippi.nix
+    ./steam.nix
+  ];
+}
--- systems/desktop.nix
+++ systems/desktop.nix
@@ -2,7 +2,7 @@
   imports = [
     ../core/desktop
     ../modules/desktop/audio-video.nix
-    ../modules/desktop/gaming.nix
+    ../modules/desktop/gaming
     ../modules/development/javascript.nix
     ../modules/hardware/multi-monitor.nix
   ];
