84e5b0bfab6ab2a2d7aab1df49f84afb1b887901
Luke Leheny <luke.leheny@pm.me>
2025-04-21 11:44:32 -0400

    Remove JDK override
    JDKs are included by default:
    https://github.com/PrismLauncher/PrismLauncher/blob/develop/nix/README.md

--- modules/desktop/gaming/minecraft-client.nix
+++ modules/desktop/gaming/minecraft-client.nix
@@ -4,7 +4,7 @@
   users.users.luke.packages = with pkgs; [
     glfw3-minecraft
     mcaselector
-    (prismlauncher.override { jdks = [ jdk21 jdk8 ]; })
+    prismlauncher
   ];
 
   home-manager.users.luke.home.file.".alsoftrc".text = ''
