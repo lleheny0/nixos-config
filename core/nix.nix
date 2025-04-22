5f462081dc5260bb865c797aac80137395346372
Luke Leheny <luke.leheny@pm.me>
2025-04-22 19:05:29 -0400

    Use systemd timers for garbage collection

--- core/nix.nix
+++ core/nix.nix
@@ -1,17 +1,42 @@
+{ pkgs, ... }:
+
 {
   nix.settings = {
     auto-optimise-store = true;
   };
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  systemd.services.nix-gc-system = {
+    description = "Garbage collection for system profiles";
+    path = with pkgs; [ nix ];
+    script = "nix-collect-garbage --delete-older-than 30d";
+    serviceConfig = {
+      Type = "oneshot";
+    };
+  };
+  systemd.timers.nix-gc-system = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      Unit = "nix-gc-system.service";
+    };
   };
 
-  home-manager.users.luke.nix.gc = {
-    automatic = true;
-    frequency = "weekly";
-    options = "--delete-older-than 30d";
+  systemd.services.nix-gc-user = {
+    description = "Garbage collection for user profiles";
+    path = with pkgs; [ nix ];
+    script = "nix-collect-garbage --delete-older-than 30d";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "luke";
+    };
+  };
+  systemd.timers.nix-gc-user = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      Unit = "nix-gc-user.service";
+    };
   };
 }
