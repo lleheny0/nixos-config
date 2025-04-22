6d0fe17ae783514585ff26b5dd9f76237d5140cc
Luke Leheny <luke.leheny@pm.me>
2025-04-22 19:13:54 -0400

    Rename nix to gc

--- core/default.nix
+++ core/default.nix
@@ -2,13 +2,13 @@
   imports = [
     ./boot.nix
     ./environment.nix
+    ./gc.nix
     ./git.nix
     ./home.nix
     ./keyd.nix
     ./locale.nix
     ./luke.nix
     ./networking.nix
-    ./nix.nix
     ./packages.nix
     ./vim.nix
   ];
