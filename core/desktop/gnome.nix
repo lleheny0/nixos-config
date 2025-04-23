6aef29c8ae9194052c60fb523549a9c5cfbf391a
Luke Leheny <luke.leheny@pm.me>
2025-04-22 22:43:35 -0400

    Consolidate xserver config

--- core/desktop/gnome.nix
+++ core/desktop/gnome.nix
@@ -1,12 +1,16 @@
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+    printing.enable = true;
   };
-  services.printing.enable = true;
 
   home-manager.users.luke.qt = {
     enable = true;
