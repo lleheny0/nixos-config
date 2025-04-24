baec80e6b3fbb6e2626e2c507821c0cfa43ed5e2
Luke Leheny <luke.leheny@pm.me>
2025-04-24 14:18:21 -0400

    Use KDE Classic cursors

--- core/desktop/cursor.nix
+++ core/desktop/cursor.nix
@@ -1,10 +1,25 @@
 { pkgs, lib, ... }:
 
-{
+let
+  kde-classic-cursors = pkgs.stdenv.mkDerivation {
+    name = "kde-classic-cursors";
+    src = pkgs.fetchFromGitHub {
+      owner = "KDE";
+      repo = "oxygen";
+      rev = "master";
+      hash = "sha256-jNAQ+QTA0mIuXWbE8QRsFagfJcCbJH/c2rJG3nTAeP0=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/icons/KDE_Classic
+      cp -r $src/cursors/KDE_Classic/* $out/share/icons/KDE_Classic/
+    '';
+  };
+in {
   home-manager.users.luke = {
     home.pointerCursor = {
-      name = "Adwaita";
-      package = pkgs.adwaita-icon-theme;
+      name = "KDE_Classic";
+      package = kde-classic-cursors;
       size = 24;
       gtk.enable = true;
       x11.enable = true;
@@ -12,7 +27,7 @@
 
     dconf.settings = {
       "org/gnome/desktop/interface" = {
-        cursor-theme = lib.mkDefault "Adwaita";
+        cursor-theme = lib.mkDefault "KDE_Classic";
       };
     };
   };
