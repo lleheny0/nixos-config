09e835e9813ca445dd74e922d3d46885d5749bb1
Luke Leheny <luke.leheny@pm.me>
2025-05-05 13:54:21 -0400

    Update vim config

--- core/vim.nix
+++ core/vim.nix
@@ -1,21 +1,27 @@
+{ pkgs, ... }:
+
 {
-  home-manager.users.luke.programs.vim = {
+  programs.vim = {
     enable = true;
-    extraConfig = ''
-      set number
-      set expandtab
-      set smarttab
-      set shiftwidth=2
-      set tabstop=2
-      set ai
-      set si
-      set wrap
-      syntax on
-      set mouse=a
-      set clipboard=unnamedplus
-      set nobackup
-      set nowb
-      set noswapfile
-    '';
+    defaultEditor = true;
+    package = (pkgs.vim_configurable.override {  }).customize{
+      name = "vim";
+      vimrcConfig.customRC = ''
+        set number
+        set expandtab
+        set smarttab
+        set shiftwidth=2
+        set tabstop=2
+        set ai
+        set si
+        set wrap
+        syntax on
+        set mouse=a
+        set clipboard=unnamedplus
+        set nobackup
+        set nowb
+        set noswapfile
+      '';
+    };
   };
 }
