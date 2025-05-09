a63ced971aa3a35ad647fb22498d0c85da6b0660
Luke Leheny <luke.leheny@pm.me>
2025-05-09 11:43:50 -0400

    Update chromium settings

--- core/desktop/applications/chromium.nix
+++ core/desktop/applications/chromium.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+# Settings that need to be manually set:
+# 1. chrome://settings/performance > Preload pages
+# 2. chrome://settings/appearance > Theme
+# 3. chrome://settings/security > Warn you if a password was compromised
+# 4. chrome://settings/content/siteData > Delete data when exiting
 
 {
   programs.chromium = {
@@ -6,6 +10,15 @@
     extraOpts = {
       "AutofillAddressEnabled" = false;
       "AutofillCreditCardEnabled" = false;
+      "BlockThirdPartyCookies" = true;
+      "ClearBrowsingDataOnExitList" = [
+        "browsing_history"
+        "download_history"
+        "cached_images_and_files"
+        "password_signin"
+        "autofill"
+        "hosted_app_data"
+      ];
       "ForceMaximizeOnFirstRun" = true;
       "PasswordManagerEnabled" = false;
       "RestoreOnStartup" = 1;
@@ -23,6 +36,7 @@
     commandLineArgs = [
       "--enable-features=AcceleratedVideoEncoder"
       "--enable-features=FluentOverlayScrollbars,FluentOverlayScrollbar"
+      "--disable-background-networking"
       "--ignore-gpu-blocklist"
       "--enable-zero-copy"
     ];
