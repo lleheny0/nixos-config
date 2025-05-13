164556ea9a7426c2c2b7c38e2109e4e47f78bac4
Luke Leheny <luke.leheny@pm.me>
2025-05-13 12:04:34 -0400

    Move gnome-related packages to gnome.nix

--- core/desktop/gnome.nix
+++ core/desktop/gnome.nix
@@ -21,4 +21,12 @@
     };
     style.name = "adwaita-dark";
   };
+
+  users.users.luke.packages = with pkgs; [
+    adwaita-qt
+    dconf-editor
+    gnome-tweaks
+    gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-dock
+  ];
 }
--- core/desktop/packages.nix
+++ core/desktop/packages.nix
@@ -2,16 +2,11 @@
 
 {
   users.users.luke.packages = with pkgs; [
-    adwaita-qt
     beeper
-    dconf-editor
     discord
     exiftool
     fragments
     gimp
-    gnome-tweaks
-    gnomeExtensions.appindicator
-    gnomeExtensions.dash-to-dock
     libva-utils
     radeontop
     signal-desktop-bin
