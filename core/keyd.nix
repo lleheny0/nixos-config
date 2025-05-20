9d8b85277e3e4eea9488a1ebc5eb8b5aedde7561
Luke Leheny <luke.leheny@pm.me>
2025-05-19 21:48:35 -0400

    Comment out keybindings I rarely use

--- core/keyd.nix
+++ core/keyd.nix
@@ -43,13 +43,13 @@
             "]" = "C-pagedown";
             j = "C-S-j";
           };
-          "aux+shift" = {
-            f1 = "C-S-f1";
-            "2" = "C-S-2";
-            x = "C-S-x";
-            s = "C-S-s";
-            w = "C-S-w";
-          };
+          # "aux+shift" = {
+            # f1 = "C-S-f1";
+            # "2" = "C-S-2";
+            # x = "C-S-x";
+            # s = "C-S-s";
+            # w = "C-S-w";
+          # };
           "meta+shift" = {
             s = "sysrq";
           };
