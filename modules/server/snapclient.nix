e95c61a23912be1f71890dc5800839090eb1f4c2
Luke Leheny <luke.leheny@pm.me>
2025-05-24 23:08:57 -0400

    Run snapclient as the user after login

--- modules/server/snapclient.nix
+++ modules/server/snapclient.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 
 {
-  systemd.services.snapclient = {
+  systemd.user.services.snapclient = {
+    enable = true;
     description = "Snapcast client";
+    after = [ "network.target" ];
+    wantedBy = [ "default.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.snapcast}/bin/snapclient --host 192.168.1.128 --soundcard 3";
+      ExecStart = "${pkgs.snapcast}/bin/snapclient --host 192.168.1.128";
       Restart = "on-failure";
     };
   };
