b17749f357ddccb66776ca981e998e1330279ae3
Luke Leheny <luke.leheny@pm.me>
2025-05-27 19:57:14 -0400

    Add volume normalization
    Add delay to service
    Add librespot package for debugging

--- modules/server/snapserver.nix
+++ modules/server/snapserver.nix
@@ -33,10 +33,15 @@
     after = [ "network.target" ];
     wantedBy = [ "default.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.librespot}/bin/librespot --backend pipe --device /run/spotify --name Home --zeroconf-port 15353";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 30";
+      ExecStart = "${pkgs.librespot}/bin/librespot --backend pipe --device /run/spotify --name Home --zeroconf-port 15353 --enable-volume-normalisation --normalisation-method basic";
       Restart = "on-failure";
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    librespot
+  ];
 
   networking.firewall.allowedTCPPorts = [ 1704 1705 1780 5353 15353 ];
 }
