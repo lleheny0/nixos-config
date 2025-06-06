9c9facd70800f827b76072cc6a0ad130b1f0e428
Luke Leheny <luke.leheny@pm.me>
2025-06-05 23:35:59 -0400

    Enable AV properties in nautilus

--- core/desktop/applications/nautilus.nix
+++ core/desktop/applications/nautilus.nix
@@ -1,8 +1,23 @@
+{ pkgs, ... }:
+
 {
   programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "alacritty";
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      nautilus = prev.nautilus.overrideAttrs (nprev: {
+        buildInputs =
+          nprev.buildInputs
+          ++ (with pkgs.gst_all_1; [
+            gst-plugins-good
+            gst-plugins-bad
+          ]);
+      });
+    })
+  ];
 
   home-manager.users.luke.dconf.settings = {
     "org/gnome/nautilus/icon-view" = {
