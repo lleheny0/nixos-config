70e80880b17ddeb471af5b565c295f717e07f42e
Luke Leheny <luke.leheny@pm.me>
2025-06-12 22:54:24 -0400

    Use cubic volume control

--- modules/server/snapcast/server.nix
+++ modules/server/snapcast/server.nix
@@ -25,7 +25,7 @@
     after = [ "network-online.target" ];
     wantedBy = [ "default.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.librespot}/bin/librespot --backend pipe --device /run/spotify --name Home --zeroconf-port 15353 --enable-volume-normalisation --normalisation-method basic";
+      ExecStart = "${pkgs.librespot}/bin/librespot --backend pipe --device /run/spotify --name Home --zeroconf-port 15353 --enable-volume-normalisation --normalisation-method basic --volume-ctrl cubic";
       Restart = "on-failure";
       User = "luke";
     };
