6b6b4590e7b785a5f5b0a2ff1379093365372bd2
Luke Leheny <luke.leheny@pm.me>
2025-06-17 01:13:22 -0400

    Update Minecraft backup
    It was creating an archive with an extra directory named "."

--- modules/server/minecraft/backup.nix
+++ modules/server/minecraft/backup.nix
@@ -5,7 +5,8 @@
     description = "Minecraft world backup";
     path = with pkgs; [ gnutar gzip ];
     script = ''
-      tar -czf "/home/luke/Backups/Minecraft/$(date +%Y-%m-%d).tar.gz" -C /home/luke/Minecraft .
+      cd /home/luke/Minecraft
+      tar -czf "/home/luke/Backups/Minecraft/$(date +%Y-%m-%d).tar.gz" *
       ls -F /home/luke/Backups/Minecraft/*.tar.gz | head -n -10 | xargs -r rm
       cp -rnv /home/luke/Backups/Minecraft /backup
       ls -F /backup/Minecraft/*.tar.gz | head -n -10 | xargs -r rm
