f9f3bd29194259724a0a7ae7cf65bdc92995170c
Luke Leheny <luke.leheny@pm.me>
2025-06-21 09:56:22 -0400

    Move librespot to its own file and remove restart
    Librespot doesn't work right at boot and needs to be manually restarted.
    The restarter service was working sometimes but not reliably. Will
    investigate further in the future.

---
+++ modules/server/snapcast/librespot.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+# Note: The librespot service must be manually restarted on boot
+
+{
+  systemd.services.librespot = {
+    enable = true;
+    description = "Librespot";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.librespot}/bin/librespot --backend pipe --device /run/snapcast --name Home --zeroconf-port 15353 --enable-volume-normalisation --normalisation-method basic --volume-ctrl cubic";
+      Restart = "on-failure";
+      User = "luke";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 5353 15353 ];
+}
--- modules/server/snapcast/server.nix
+++ modules/server/snapcast/server.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }:
 
 {
+  imports = [
+    ./librespot.nix
+  ];
+
   systemd.tmpfiles.rules = [
-    "p /run/spotify 0666 luke users -"
+    "p /run/snapcast 0666 luke users -"
   ];
 
   services.snapserver = {
@@ -11,7 +15,7 @@
     streams = {
       spotify = {
         type = "pipe";
-        location = "/run/spotify";
+        location = "/run/snapcast";
         query = {
           sampleformat = "44100:16:2";
         };
@@ -19,37 +23,5 @@
     };
   };
 
-  systemd.services.librespot = {
-    enable = true;
-    description = "Librespot";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "default.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.librespot}/bin/librespot --backend pipe --device /run/spotify --name Home --zeroconf-port 15353 --enable-volume-normalisation --normalisation-method basic --volume-ctrl cubic";
-      Restart = "on-failure";
-      User = "luke";
-    };
-  };
-
-  systemd.services.librespot-restarter = {
-    script = "${pkgs.systemd}/bin/systemctl restart librespot";
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-  };
-  systemd.timers.librespot-restarter = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnBootSec = "120";
-      Unit = "librespot-restarter.service";
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    librespot
-  ];
-
-  networking.firewall.allowedTCPPorts = [ 1704 1705 1780 5353 15353 ];
+  networking.firewall.allowedTCPPorts = [ 1704 1705 1780 ];
 }
