bf1f1b4be4100199bd7913f70d4070493656b1da
Luke Leheny <luke.leheny@pm.me>
2025-06-29 16:17:05 -0400

    Add scripts for testing vscode settings

--- core/desktop/applications/vscode.nix
+++ core/desktop/applications/vscode.nix
@@ -212,4 +212,26 @@
       };
     };
   };
+
+  # Scripts to lock and unlock settings for quick testing
+  users.users.luke.packages = with pkgs; [
+    (writeShellApplication {
+      name = "vsu";
+      text = ''
+        cd /home/luke/.config/Code/User
+        mv settings.json settings.json.link
+        cp settings.json.link settings.json
+        cd ~-
+      '';
+    })
+    (writeShellApplication {
+      name = "vsl";
+      text = ''
+        cd /home/luke/.config/Code/User
+        rm settings.json
+        mv settings.json.link settings.json
+        cd ~-
+      '';
+    })
+  ];
 }
