750a6624fd54b655bcc09c116d08db4f5a33e8e0
Luke Leheny <luke.leheny@pm.me>
2025-06-30 22:47:36 -0400

    Remove extra nest

--- core/desktop/gnome/wallpaper.nix
+++ core/desktop/gnome/wallpaper.nix
@@ -2,20 +2,18 @@ let
   wallpaper = "file://" + ./wallpaper.jpg;
 in
 {
-  home-manager.users.luke = {
-    dconf.settings = {
-      "org/gnome/desktop/background" = {
-        color-shading-type = "solid";
-        picture-options = "zoom";
-        picture-uri = wallpaper;
-        picture-uri-dark = wallpaper;
-      };
-      "org/gnome/desktop/screensaver" = {
-        color-shading-type = "solid";
-        picture-options = "zoom";
-        picture-uri = wallpaper;
-        picture-uri-dark = wallpaper;
-      };
+  home-manager.users.luke.dconf.settings = {
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = wallpaper;
+      picture-uri-dark = wallpaper;
+    };
+    "org/gnome/desktop/screensaver" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = wallpaper;
+      picture-uri-dark = wallpaper;
     };
   };
 }
