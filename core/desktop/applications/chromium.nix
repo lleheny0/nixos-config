f273cad0a93bf5654003634b9eaecd6de0532234
Luke Leheny <luke.leheny@pm.me>
2025-07-06 13:14:55 -0400

    Disable shortcut popup on every launch

--- core/desktop/applications/chromium.nix
+++ core/desktop/applications/chromium.nix
@@ -45,6 +45,7 @@
       "--use-angle=vulkan"
       "--enable-accelerated-video-decode"
       "--enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport,FluentOverlayScrollbars,FluentOverlayScrollbar"
+      "--disable-features=GlobalShortcutsPortal"
     ];
   };
 }
