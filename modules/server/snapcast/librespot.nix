6ecde23ba818b08596e75d3bf3b784b21e0259a8
Luke Leheny <luke.leheny@pm.me>
2025-08-08 00:34:15 -0400

    Add overlay with fix for Spotify API update
    https://github.com/librespot-org/librespot/issues/1527
    https://github.com/librespot-org/librespot/pull/1528
    https://discourse.nixos.org/t/is-it-possible-to-override-cargosha256-in-buildrustpackage/4393

--- modules/server/snapcast/librespot.nix
+++ modules/server/snapcast/librespot.nix
@@ -3,6 +3,28 @@
 # Note: The librespot service must be manually restarted on boot
 
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      librespot = prev.librespot.overrideAttrs (oldAttrs: {
+        src = prev.fetchFromGitHub {
+          owner = "librespot-org";
+          repo = "librespot";
+          rev = "3c9e84a68e8686c96ece937c531157940acac206";
+          sha256 = "sha256-U2sLiy5R1a0JC76bhqaR+maBV13ghUsBsruS03CXP3M=";
+        };
+        cargoDeps = final.rustPlatform.fetchCargoVendor {
+          src = prev.fetchFromGitHub {
+            owner = "librespot-org";
+            repo = "librespot";
+            rev = "3c9e84a68e8686c96ece937c531157940acac206";
+            sha256 = "sha256-U2sLiy5R1a0JC76bhqaR+maBV13ghUsBsruS03CXP3M=";
+          };
+          hash = "sha256-Lujz2revTAok9B0hzdl8NVQ5XMRY9ACJzoQHIkIgKMg=";
+        };
+      });
+    })
+  ];
+
   systemd.services.librespot = {
     enable = true;
     description = "Librespot";
