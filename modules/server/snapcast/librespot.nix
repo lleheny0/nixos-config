776e0d58d8d3804902e2f0054bc1a868f1d6d5cb
Luke Leheny <luke.leheny@pm.me>
2025-08-10 10:49:56 -0400

    Use host blocking workaround instead of overlay
    https://github.com/librespot-org/librespot/issues/1527

--- modules/server/snapcast/librespot.nix
+++ modules/server/snapcast/librespot.nix
@@ -3,28 +3,6 @@
 # Note: The librespot service must be manually restarted on boot
 
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      librespot = prev.librespot.overrideAttrs (oldAttrs: {
-        src = prev.fetchFromGitHub {
-          owner = "librespot-org";
-          repo = "librespot";
-          rev = "3c9e84a68e8686c96ece937c531157940acac206";
-          sha256 = "sha256-U2sLiy5R1a0JC76bhqaR+maBV13ghUsBsruS03CXP3M=";
-        };
-        cargoDeps = final.rustPlatform.fetchCargoVendor {
-          src = prev.fetchFromGitHub {
-            owner = "librespot-org";
-            repo = "librespot";
-            rev = "3c9e84a68e8686c96ece937c531157940acac206";
-            sha256 = "sha256-U2sLiy5R1a0JC76bhqaR+maBV13ghUsBsruS03CXP3M=";
-          };
-          hash = "sha256-Lujz2revTAok9B0hzdl8NVQ5XMRY9ACJzoQHIkIgKMg=";
-        };
-      });
-    })
-  ];
-
   systemd.services.librespot = {
     enable = true;
     description = "Librespot";
@@ -40,5 +18,11 @@
 
   environment.shellAliases.rl = "sudo systemctl restart librespot.service";
 
-  networking.firewall.allowedTCPPorts = [ 5353 15353 ];
+  networking = {
+    firewall.allowedTCPPorts = [ 5353 15353 ];
+
+    hosts = {
+      "0.0.0.0" = [ "apresolve.spotify.com" ];
+    };
+  };
 }
