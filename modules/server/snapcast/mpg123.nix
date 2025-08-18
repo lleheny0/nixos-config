c876fc2b7d897218d91c478ec92b1547c64d43b8
Luke Leheny <luke.leheny@pm.me>
2025-08-18 14:29:46 -0400

    Shorten script names

--- modules/server/snapcast/mpg123.nix
+++ modules/server/snapcast/mpg123.nix
@@ -4,7 +4,7 @@
   users.users.luke.packages = with pkgs; [
     mpg123
     (writeShellApplication {
-      name = "svgm";
+      name = "vgm";
       runtimeInputs = [ mpg123 tmux ];
       text = ''
         tmux new-session -d -s vgmradio 'mpg123 -s https://leheny.ddns.net/vgmradio > /run/snapcast'
@@ -13,7 +13,7 @@
       '';
     })
     (writeShellApplication {
-      name = "wmvy";
+      name = "mvy";
       runtimeInputs = [ mpg123 tmux ];
       text = ''
         tmux new-session -d -s wmvy 'mpg123 -s https://mvyradio.streamguys1.com/mvy-mp3 > /run/snapcast'
