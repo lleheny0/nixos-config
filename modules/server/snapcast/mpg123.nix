a8d600f034f20b2f11b25224443c6131a9dae286
Luke Leheny <luke.leheny@pm.me>
2025-08-18 11:27:28 -0400

    Add MVY radio stream

--- modules/server/snapcast/mpg123.nix
+++ modules/server/snapcast/mpg123.nix
@@ -12,5 +12,14 @@
         tmux attach -t vgmradio
       '';
     })
+    (writeShellApplication {
+      name = "wmvy";
+      runtimeInputs = [ mpg123 tmux ];
+      text = ''
+        tmux new-session -d -s wmvy 'mpg123 -s https://mvyradio.streamguys1.com/mvy-mp3 > /run/snapcast'
+        tmux send-keys -t wmvy 'u--------------------u' Enter
+        tmux attach -t wmvy
+      '';
+    })
   ];
 }
