e9e56ebf770038dfb0de9a8dd1825deab0e5fb10
Luke Leheny <luke.leheny@pm.me>
2025-08-18 11:26:47 -0400

    Update vgm stream with automatic volume adjustment

--- modules/server/snapcast/mpg123.nix
+++ modules/server/snapcast/mpg123.nix
@@ -4,10 +4,12 @@
   users.users.luke.packages = with pkgs; [
     mpg123
     (writeShellApplication {
-      name = "vs";
-      runtimeInputs = [ mpg123 ];
+      name = "svgm";
+      runtimeInputs = [ mpg123 tmux ];
       text = ''
-        mpg123 -s https://leheny.ddns.net/vgmradio > /run/snapcast
+        tmux new-session -d -s vgmradio 'mpg123 -s https://leheny.ddns.net/vgmradio > /run/snapcast'
+        tmux send-keys -t vgmradio 'u--------------------u' Enter
+        tmux attach -t vgmradio
       '';
     })
   ];
