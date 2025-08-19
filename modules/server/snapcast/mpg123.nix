106f8b537d454cbeda576b0ca5057c864df93f4f
Luke Leheny <luke.leheny@pm.me>
2025-08-18 22:27:49 -0400

    Lower starting volume on WMVY

--- modules/server/snapcast/mpg123.nix
+++ modules/server/snapcast/mpg123.nix
@@ -17,7 +17,7 @@
       runtimeInputs = [ mpg123 tmux ];
       text = ''
         tmux new-session -d -s wmvy 'mpg123 -s https://mvyradio.streamguys1.com/mvy-mp3 > /run/snapcast'
-        tmux send-keys -t wmvy 'u--------------------u' Enter
+        tmux send-keys -t wmvy 'u-------------------------u' Enter
         tmux attach -t wmvy
       '';
     })
