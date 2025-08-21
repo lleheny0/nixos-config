29193dc4fa60d5e4c9847e80476e18f85b4f76ac
Luke Leheny <luke.leheny@pm.me>
2025-08-21 09:45:37 -0400

    Lower volume for VGM stream

--- modules/server/snapcast/mpg123.nix
+++ modules/server/snapcast/mpg123.nix
@@ -8,7 +8,7 @@
       runtimeInputs = [ mpg123 tmux ];
       text = ''
         tmux new-session -d -s vgmradio 'mpg123 -s https://leheny.ddns.net/vgmradio > /run/snapcast'
-        tmux send-keys -t vgmradio 'u--------------------u' Enter
+        tmux send-keys -t vgmradio 'u-------------------------u' Enter
         tmux attach -t vgmradio
       '';
     })
