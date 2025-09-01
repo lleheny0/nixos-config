c105b65ce8012b3e1bd3a465c7291e74e335eaca
Luke Leheny <luke.leheny@pm.me>
2025-09-01 13:06:49 -0400

    Move BOTW backup into cemu config

--- modules/desktop/gaming/cemu.nix
+++ modules/desktop/gaming/cemu.nix
@@ -10,10 +10,6 @@
 # in
 
 {
-  imports = [
-    ./botw-backup.nix
-  ];
-
   boot.kernelModules = [
     "hid-nintendo"
   ];
@@ -25,4 +21,26 @@
   ];
 
   environment.shellAliases.jc = "sudo joycond-cemuhook";
+
+  systemd.services.botw-backup = {
+    description = "BOTW save backup";
+    path = with pkgs; [ gnutar gzip ];
+    script = ''
+      cd /home/luke/.local/share/Cemu/mlc01/usr/save/00050000/101c9400/user/80000001
+      tar -czf "/home/luke/Backups/BOTW/$(date +%Y-%m-%d).tar.gz" *
+      ls -F /home/luke/Backups/BOTW/*.tar.gz | head -n -10 | xargs -r rm
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "luke";
+    };
+  };
+  systemd.timers.botw-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 04:00:00";
+      Persistent = true;
+      Unit = "botw-backup.service";
+    };
+  };
 }
