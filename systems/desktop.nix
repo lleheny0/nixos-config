c240c12ee3cb235a7c2989f5d78237e1eb527bfb
Luke Leheny <luke.leheny@pm.me>
2025-10-03 16:18:46 -0400

    Add work module for slack

--- systems/desktop.nix
+++ systems/desktop.nix
@@ -4,6 +4,7 @@
     ../modules/desktop/multimedia.nix
     ../modules/desktop/gaming
     ../modules/desktop/social.nix
+    ../modules/desktop/work.nix
     ../modules/development/javascript.nix
     ../modules/hardware/ecotank.nix
     ../modules/hardware/multi-monitor.nix
