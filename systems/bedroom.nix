b3376c869efe727bf8a01b5df0aca023e07bef66
Luke Leheny <luke.leheny@pm.me>
2025-10-26 10:40:53 -0400

    Update bedroom system for new chromebox

--- systems/bedroom.nix
+++ systems/bedroom.nix
@@ -4,16 +4,11 @@
     ../modules/server/snapcast/client.nix
   ];
 
+  gc.deleteOlderThan = "7d";
+
   snapclient = {
     asUser = false;
-    params = "--soundcard 2 --latency 25";
-  };
-
-  services.thinkfan = {
-    enable = true;
-    levels = [
-      [1 0 255]
-    ];
+    params = "--soundcard 2";
   };
 
   networking.hostName = "bedroom";
