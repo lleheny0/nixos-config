335e8bdc623a3b313c7444ac7af5e50233368e6e
Luke Leheny <luke.leheny@pm.me>
2026-02-10 20:10:11 -0500

    Add command to restore firefox profile

--- core/desktop/environment.nix
+++ core/desktop/environment.nix
@@ -4,6 +4,7 @@
       dw = "dconf watch /";
       ir = "exiftool '-filename<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le' .";
       mb = "cd /home/luke && rm -rf .mozilla.backup && mkdir .mozilla.backup && cp -r .mozilla/* .mozilla.backup && cd ~-";
+      mr = "cd /home/luke && rm -rf .mozilla && mkdir .mozilla && cp -r .mozilla.backup/* .mozilla && cd ~-";
       npub = "mb && np && nu && nb";
       npus = "mb && np && nu && ns";
       pa = "for file in *.tex; do pdflatex \"$file\"; done";
