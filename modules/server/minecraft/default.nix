f530e32ed79122d219eb164166b2e263ed707b67
Luke Leheny <luke.leheny@pm.me>
2026-03-21 22:19:07 -0400

    Add prerequisite notes

--- modules/server/minecraft/default.nix
+++ modules/server/minecraft/default.nix
@@ -1,3 +1,9 @@
+# Prerequisites:
+# 1. Mount second drive to /backup and set luke:users as owner
+# 2. Create /home/luke/Minecraft directory
+# 3. Create /home/luke/Backups/Minecraft directory
+# 4. Create /backups/Minecraft directory
+
 {
   imports = [
     ./backup.nix
--- modules/server/vgm-radio/default.nix
+++ modules/server/vgm-radio/default.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 
+# Prerequisites:
+# 1. Mount second drive to /backup and set luke:users as owner
+# 2. Create /vgm and set luke:users as owner
+# 3. Copy all music to Music directory in /vgm
+# 4. Clone vgm-radio repo in /vgm
+# 5. Create /backup/Music directory
+
 {
   imports = [
     ./backup.nix
