88b870cf45f3e5493f543288b6a954b286194437
Luke Leheny <luke.leheny@pm.me>
2026-05-24 19:17:37 -0400

    Lower default volume on mpg123 streams

--- modules/server/snapcast/mpg123.nix
+++ modules/server/snapcast/mpg123.nix
@@ -8,7 +8,7 @@
       runtimeInputs = [ mpg123 tmux ];
       text = ''
         tmux new-session -d -s vgmradio 'mpg123 -s https://leheny.ddns.net/vgmradio > /run/snapcast'
-        tmux send-keys -t vgmradio 'u-------------------------u' Enter
+        tmux send-keys -t vgmradio 'u------------------------------u' Enter
         tmux attach -t vgmradio
       '';
     })
@@ -17,7 +17,7 @@
       runtimeInputs = [ mpg123 tmux ];
       text = ''
         tmux new-session -d -s wmvy 'mpg123 -s https://mvyradio.streamguys1.com/mvy-mp3 > /run/snapcast'
-        tmux send-keys -t wmvy 'u-------------------------u' Enter
+        tmux send-keys -t wmvy 'u------------------------------u' Enter
         tmux attach -t wmvy
       '';
     })
