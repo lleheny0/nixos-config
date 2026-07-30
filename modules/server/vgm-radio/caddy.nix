f783c9885a73d6e0e689c1b50d04ebfb4597700b
Luke Leheny <luke.leheny@pm.me>
2026-07-30 15:26:28 -0400

    Add resume and homepage to caddy

--- modules/server/vgm-radio/caddy.nix
+++ modules/server/vgm-radio/caddy.nix
@@ -4,11 +4,19 @@
   services.caddy = {
     enable = true;
     virtualHosts."lukeleheny.com".extraConfig = ''
+      root * /www/homepage
+      file_server
+
       handle_path /vgmradio* {
         root * /vgm/vgm-radio
         file_server
         reverse_proxy /icecast* http://localhost:8000
         reverse_proxy /metadata* http://localhost:8080
+      }
+
+      handle_path /resume* {
+        root * /www/resume
+        file_server
       }
     '';
   };
